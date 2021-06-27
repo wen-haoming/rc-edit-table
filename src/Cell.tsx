8218f0cf51001258db0e9310730c50ed312e1118
wenhaoming <435203093@qq.com>
2021-06-27 11:26:04 +0800

    feat:点击单元格的光标问题

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { useRef,useState } from 'react';
+import React, { useRef, useState, MouseEventHandler } from 'react';
 import classNames from 'classnames';
 import marked from 'marked';
 
@@ -9,30 +9,41 @@ interface Props {
   trItem: any;
 }
 
-// 
+//
 const CellComp: React.FC<Props> = (props) => {
   const { prefixCls, trIdx, tdIdx, trItem } = props;
-  const contentRef = useRef(null)
- const [content,setContent] = useState('')
+  const contentRef = useRef<HTMLElement>(null);
+  const tdBgRef = useRef<HTMLElement>(null);
 
-
-  const handleKeydown = (e)=>{
-    if(contentRef.current && !contentRef.current.childNodes.length){
-       let Pnode =  document.createElement('p')
-        contentRef.current.append(Pnode)
-        Pnode.autofocus = true
+  const handleKeydown = (e) => {
+    //  如果该单元格内没有元素的话，第一个内容是一个p标签来填充，而不是一个 content
+    if (contentRef.current && !contentRef.current.childNodes.length) {
+      const pNode = document.createElement('p');
+      contentRef.current.append(pNode);
+      //   pNode.autofocus = true;
     }
+  };
 
-  }
-
+  const handleClickTd = (e) => {
+    if (e.target.nodeName === 'TD') {
+      const selObj = window.getSelection();
+      const length = contentRef.current?.children.length;
+      selObj?.setPosition(contentRef.current, length);
+    }
+  };
   return (
-    <td>
+    <td onClick={handleClickTd}>
       <div className={classNames(`${prefixCls}-td-content`)}>
-        <div ref={contentRef} suppressContentEditableWarning onKeyDown={handleKeydown}  contentEditable >
+        <div
+          ref={contentRef}
+          suppressContentEditableWarning
+          onKeyDown={handleKeydown}
+          contentEditable
+        >
           {/* {trItem[tdIdx]} */}
         </div>
       </div>
-      <div className={classNames(`${prefixCls}-td-bg`)}>
+      <div className={classNames(`${prefixCls}-td-bg`)} ref={tdBgRef}>
         <div
           className={classNames({
             [`${prefixCls}-border-top`]: true,
